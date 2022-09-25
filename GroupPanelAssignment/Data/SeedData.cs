0c96c34597a33928769d3b67e7616f687f271a7b
f-yankey <yankeyfrancis152@gmail.com>
2022-09-25 16:18:39 +0000

    Seeding database complete

---
+++ GroupPanelAssignment/Data/SeedData.cs
@@ -0,0 +1,90 @@
+﻿using GroupPanelAssignment.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupPanelAssignment.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new PanelTeamAssignDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<PanelTeamAssignDbContext>>()))
+            {
+                // Look for any role.
+                bool containsRole = context.Roles.Any();
+                bool containsScoreItemTypes = context.ScoreItemTypes.Any();
+                bool doesNotContainEither = !containsRole || !containsScoreItemTypes;
+
+                if (!containsRole)
+                {
+                    PopulateRoles(context);
+                }
+
+                if (!containsScoreItemTypes)
+                {
+                    PopulateScoreItemTypes(context);
+                }
+
+                if (doesNotContainEither)
+                {
+                    SaveContext(context);
+                }
+            }
+        }
+
+        private static void SaveContext(PanelTeamAssignDbContext context)
+        {
+            context.SaveChanges();
+        }
+
+        private static void PopulateScoreItemTypes(PanelTeamAssignDbContext context)
+        {
+            context.ScoreItemTypes.AddRange(
+                                    new ScoreItemType
+                                    {
+                                        ScoreItemTypeName = "Group Score Item",
+                                        Created = DateTime.Now,
+                                        CreatedBy = "admin"
+                                    },
+
+                                    new ScoreItemType
+                                    {
+                                        ScoreItemTypeName = "Individual Score Item",
+                                        Created = DateTime.Now,
+                                        CreatedBy = "admin"
+                                    }
+                                );
+        }
+
+        private static void PopulateRoles(PanelTeamAssignDbContext context)
+        {
+            context.Roles.AddRange(
+                                    new Role
+                                    {
+                                        RoleName = "Student",
+                                        Created = DateTime.Now,
+                                        CreatedBy = "admin"
+                                    },
+
+                                    new Role
+                                    {
+                                        RoleName = "Supervisor",
+                                        Created = DateTime.Now,
+                                        CreatedBy = "admin"
+                                    },
+
+                                    new Role
+                                    {
+                                        RoleName = "Panel Member",
+                                        Created = DateTime.Now,
+                                        CreatedBy = "admin"
+                                    }
+                                );
+        }
+    }
+}
--- GroupPanelAssignment/Startup.cs
+++ GroupPanelAssignment/Startup.cs
@@ -1,3 +1,4 @@
+using GroupPanelAssignment.Data;
 using GroupPanelAssignment.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -30,11 +31,22 @@ namespace GroupPanelAssignment
             (
                 options => options.UseSqlServer(Configuration.GetConnectionString("default"))
             );
+
+
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                SeedData.Initialize(services);
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
