ed39687af67505fc0b1615e4b8f5c58147e28e46
f-yankey <yankeyfrancis152@gmail.com>
2022-10-22 19:57:06 +0000

    Group management page taking form

--- GroupPanelAssignment/Pages/GroupManagement/Groups.cshtml.cs
+++ GroupPanelAssignment/Pages/GroupManagement/Groups.cshtml.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using X.PagedList;
 using GroupPanelAssignment.Data.ViewModels;
 using GroupPanelAssignment.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -13,7 +10,20 @@ namespace GroupPanelAssignment.Pages.GroupManagement
     {
         private ITeamManagementService _teamManagementService;
 
-        public List<TeamViewModel> Teams { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? PageNumber { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } = 10;
+
+        public IPagedList<TeamViewModel> Teams { get; set; }
+
+        public string PageTitle { get; set; }
+        public InputModel Input { get; set; }
+        public class InputModel
+        {
+            public string SearchText { get; set; }
+        }
 
         public GroupsModel(ITeamManagementService teamManagementService)
         {
@@ -21,7 +31,7 @@ namespace GroupPanelAssignment.Pages.GroupManagement
         }
         public void OnGet()
         {
-            Teams = _teamManagementService.GetTeams();
+            Teams = _teamManagementService.GetTeams().ToPagedList();
         }
     }
 }
--- GroupPanelAssignment/Pages/UserManagement/Users.cshtml.cs
+++ GroupPanelAssignment/Pages/UserManagement/Users.cshtml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GroupPanelAssignment.Data.Models;
 using GroupPanelAssignment.Data.ViewModels;
 using GroupPanelAssignment.Services.Interfaces;
 using GroupPanelAssignment.Utils;
